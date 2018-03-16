97f48ba07d5a9c19601e68044ba0124d5ffe430a
Krzysiek Nowak <krzysiek.nowak@onet.eu>
2018-03-16 10:57:39 +0100

    create first files

---
+++ HistoricalData.js
@@ -0,0 +1,365 @@
+getHistoricalData() {
+    return [
+        {
+            date: "2018-03-14T12:00:00Z",
+            results: [
+                {
+                    teamMemberId: "Abby",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 5
+                        },
+                        {
+                            questionId: "2",
+                            value: 3
+                        },
+                        {
+                            questionId: "3",
+                            value: 3
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Bill",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 4
+                        },
+                        {
+                            questionId: "2",
+                            value: 4
+                        },
+                        {
+                            questionId: "3",
+                            value: 4
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Castor",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 5
+                        },
+                        {
+                            questionId: "2",
+                            value: 5
+                        },
+                        {
+                            questionId: "3",
+                            value: 5
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "David",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 5
+                        },
+                        {
+                            questionId: "2",
+                            value: 3
+                        },
+                        {
+                            questionId: "3",
+                            value: 3
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Eugenia",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 2
+                        },
+                        {
+                            questionId: "2",
+                            value: 3
+                        },
+                        {
+                            questionId: "3",
+                            value: 4
+                        }
+                    ]
+                }
+            ]
+        },
+		{
+            date: "2018-03-07T12:00:00Z",
+            results: [
+                {
+                    teamMemberId: "Abby",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 5
+                        },
+                        {
+                            questionId: "2",
+                            value: 4
+                        },
+                        {
+                            questionId: "3",
+                            value: 5
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 4
+                        },
+                        {
+                            questionId: "2",
+                            value: 3
+                        },
+                        {
+                            questionId: "3",
+                            value: 4
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Castor",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 4
+                        },
+                        {
+                            questionId: "2",
+                            value: 4
+                        },
+                        {
+                            questionId: "3",
+                            value: 5
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "David",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 4
+                        },
+                        {
+                            questionId: "2",
+                            value: 4
+                        },
+                        {
+                            questionId: "3",
+                            value: 2
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Eugenia",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 3
+                        },
+                        {
+                            questionId: "2",
+                            value: 3
+                        },
+                        {
+                            questionId: "3",
+                            value: 3
+                        }
+                    ]
+                }
+            ]
+        },
+		{
+            date: "2018-02-28T12:00:00Z",
+            results: [
+                {
+                    teamMemberId: "Abby",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 5
+                        },
+                        {
+                            questionId: "2",
+                            value: 3
+                        },
+                        {
+                            questionId: "3",
+                            value: 3
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Bill",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 4
+                        },
+                        {
+                            questionId: "2",
+                            value: 4
+                        },
+                        {
+                            questionId: "3",
+                            value: 4
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Castor",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 5
+                        },
+                        {
+                            questionId: "2",
+                            value: 5
+                        },
+                        {
+                            questionId: "3",
+                            value: 5
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "David",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 5
+                        },
+                        {
+                            questionId: "2",
+                            value: 3
+                        },
+                        {
+                            questionId: "3",
+                            value: 3
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Eugenia",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 2
+                        },
+                        {
+                            questionId: "2",
+                            value: 3
+                        },
+                        {
+                            questionId: "3",
+                            value: 4
+                        }
+                    ]
+                }
+            ]
+        },
+		{
+            date: "2018-02-21T12:00:00Z",
+            results: [            
+            ]
+        },
+		{
+            date: "2018-02-14T12:00:00Z",
+            results: [
+                {
+                    teamMemberId: "Abby",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 3
+                        },
+                        {
+                            questionId: "2",
+                            value: 3
+                        },
+                        {
+                            questionId: "3",
+                            value: 4
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Bill",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 5
+                        },
+                        {
+                            questionId: "2",
+                            value: 4
+                        },
+                        {
+                            questionId: "3",
+                            value: 3
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Castor",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 5
+                        },
+                        {
+                            questionId: "2",
+                            value: 5
+                        },
+                        {
+                            questionId: "3",
+                            value: 
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "David",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 4
+                        },
+                        {
+                            questionId: "2",
+                            value: 4
+                        },
+                        {
+                            questionId: "3",
+                            value: 3
+                        }
+                    ]
+                },
+                {
+                    teamMemberId: "Eugenia",
+                    questions: [
+                        {
+                            questionId: "1",
+                            value: 1
+                        },
+                        {
+                            questionId: "3",
+                            value: 4
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+};
